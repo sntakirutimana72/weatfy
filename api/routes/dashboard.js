c2fcb78f306fef2cd702f08e83f2f89967346cd1
Steve <sntakirutimana72@gmail.com>
2023-07-29 23:17:48 +0200

    Remove unnecessary files

--- api/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import Dashboard from '../handlers/dashboard';
-
-const router = express.Router();
-
-router.get('/', Dashboard.home);
-
-export default router;
--- app.js
+++
@@ -1,7 +0,0 @@
-import app from './api/server';
-
-const port = process.env.HOST_PORT || 3000 ;
-
-app.listen(port, () =>
-  console.log(`Englass server started at port - ${port}`)
-);
